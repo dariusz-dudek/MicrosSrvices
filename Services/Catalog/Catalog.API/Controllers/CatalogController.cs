c510e9efc81c27cee9294d9176cf7e6683d5b7c0
Dariusz Dudek <doodowicz@gmail.com>
2023-12-19 22:20:02 +0100

    Add dependency injection in program.cs

---
+++ Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,8 @@
+﻿using Catalog.API.Controllers;
+
+namespace Catalog.API.Controllers;
+
+public class CatalogController : ApiController
+{
+
+}
--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,12 @@
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Reflection;
+using Catalog.Application.Handlers;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure;
+using Catalog.Infrastructure.Data;
+using Catalog.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +14,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning();
+builder.Services.AddHealthChecks()
+    .AddMongoDb(builder.Configuration["DatabaseSettings:ConnectionString"], "Catalog Mongo Db Health Check", 
+    HealthStatus.Degraded);
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+});
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMediatR(typeof(CreateProductHandler).GetTypeInfo().Assembly);
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IBrandRepository, ProductRepository>();
+builder.Services.AddScoped<ITypeRepository, ProductRepository>();
+
 
 var app = builder.Build();
 
